f82b272857227ec90937513b327efdc3d1f523d8
Kareemhossa <110343362+Kareemhossa@users.noreply.github.com>
2023-08-22 21:43:19 +0300

    Update firebase.js
    file update

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXgmFRK8K1I0tXnPyJAXu0Ivur2zDyu7U",
-  authDomain: "auth-register-1727a.firebaseapp.com",
-  projectId: "auth-register-1727a",
-  storageBucket: "auth-register-1727a.appspot.com",
+  apiKey: "AIzaSyr2zDyu7U",
+  authDomain: "auth-regebaseapp.com",
+  projectId: "auth-reg",
+  storageBucket: "auth-register-1pspot.com",
   messagingSenderId: "159212880206",
-  appId: "1:159212880206:web:1203e52f385d4ac39a9e1f",
+  appId: "1:15921d4ac39a9e1f",
   measurementId: "G-XQVH9QCC79",
 };
 
